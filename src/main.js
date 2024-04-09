80dd691b1227cf2c43fec0a0ac6b5f775860df97
wzm1234567 <351895714@qq.com>
2024-04-09 16:47:40 +0800

    'feat:创建基本项目'

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-
-Vue.config.productionTip = false;
+import Vue from "vue"
+import App from "./App.vue"
+import router from "./router"
+import store from "./store"
+import { Button, Select } from "element-ui"
+Vue.config.productionTip = false
+Vue.use(Button)
+Vue.use(Select)
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: (h) => h(App),
-}).$mount("#app");
+  render: (h) => h(App)
+}).$mount("#app")
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,52 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require("@vue/cli-service")
+const path = require("path")
+console.log(process.env.VUE_APP_BASE_API)
+// eslint-disable-next-line no-unused-vars
+const resolve = (dir) => path.join(__dirname, ".", dir)
+console.log(process.env.VUE_APP_BASE_URL)
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-});
+  publicPath: "/",
+  outputDir: "distsss",
+  assetsDir: "assets",
+  devServer: {
+    // historyApiFallback: true,
+    // allowedHosts: "all",
+    host: "0.0.0.0",
+    https: false,
+    open: true,
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_BASE_URL,
+        changeOrigin: true,
+        secure: false,
+        pathRewrite: {
+          ["^" + process.env.VUE_APP_BASE_API]: ""
+        }
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      extensions: [],
+      alias: {
+        "@compoent": resolve("src/compoents"),
+        vue$: "vue/dist/vue.esm.js"
+      }
+    }
+  },
+  chainWebpack(config) {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap((options) => {
+        options.compilerOptions.preserveWhitespace = true
+        return options
+      })
+      .end()
+    // eslint-disable-next-line no-undef
+    config.plugin("gotHash").use(HotHashWebpackPlugin, [{ version: "1.0.0" }])
+  }
+})
